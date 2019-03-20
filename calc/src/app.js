fd9e4d6f65f12665dddacc3aa810ee3128eebb6c
Jakub Bieńkowski <jbienkowski311@gmail.com>
2019-03-20 21:16:14 +0100

    Interconnect calc, add and sub microservices

--- calc/src/app.js
+++ calc/src/app.js
@@ -1,6 +1,10 @@
+const axios = require('axios');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+const additionHost = 'addition:8080';
+const subtractionHost = 'subtraction:8080';
 
 app.use(express.json());
 
@@ -14,13 +18,25 @@ app.get('/', (req, res) => res.send({
     ]
 }));
 
-app.get('/add/:a/:b', (req, res) => res.send({
-    result: parseInt(req.params.a) + parseInt(req.params.b)
-}));
+app.get('/add/:a/:b', (req, res) => {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
 
-app.get('/sub/:a/:b', (req, res) => res.send({
-    result: parseInt(req.params.a) - parseInt(req.params.b)
-}));
+    axios.get(`http://${additionHost}/add/${a}/${b}`)
+        .then(response => res.send({
+            result: response.data.result,
+        }));
+});
+
+app.get('/sub/:a/:b', (req, res) => {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+
+    axios.get(`http://${subtractionHost}/sub/${a}/${b}`)
+        .then(response => res.send({
+            result: response.data.result,
+        }));
+});
 
 app.get('/mul/:a/:b', (req, res) => res.send({
     result: parseInt(req.params.a) * parseInt(req.params.b)
